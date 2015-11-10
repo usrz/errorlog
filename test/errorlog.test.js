974f4c72075fa1ff4bb729af0a2d007a9ab3b3f0
Pier Fumagalli <pier@usrz.com>
2015-11-10 17:11:08 +0900

    Test level parsing.

--- test/errorlog.test.js
+++ test/errorlog.test.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var expect = require('chai').expect;
-var simplelog = require('../src/errorlog.js');
+var errorlog = require('../src/errorlog.js');
 // No color test!
-simplelog.defaultColorize = false;
+errorlog.defaultColorize = false;
 
 // A regular expression matching an ISO date
 var expr = /^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}\.[\d]{3}Z - (.*)/;
@@ -24,24 +24,24 @@ function Stream() {
 }
 
 // Inject the default stream
-var stream = simplelog.defaultLog = new Stream();
+var stream = errorlog.defaultLog = new Stream();
 
 // The shared log for testing
-var log = simplelog();
+var log = errorlog();
 
 describe('Simple log test', function() {
 
   it('should expose some basic properties', function() {
-    expect(simplelog).to.be.a('function');
-    expect(simplelog.ALL  ).to.equal( -1);
-    expect(simplelog.TRACE).to.equal(  0);
-    expect(simplelog.DEBUG).to.equal(100);
-    expect(simplelog.INFO ).to.equal(200);
-    expect(simplelog.WARN ).to.equal(300);
-    expect(simplelog.ERROR).to.equal(400);
-    expect(simplelog.OFF  )  .to.equal(Number.MAX_SAFE_INTEGER);
-    expect(simplelog.defaultLog).to.be.a('function');
-    expect(simplelog.defaultLevel).to.equal(simplelog.INFO);
+    expect(errorlog).to.be.a('function');
+    expect(errorlog.ALL  ).to.equal( -1);
+    expect(errorlog.TRACE).to.equal(  0);
+    expect(errorlog.DEBUG).to.equal(100);
+    expect(errorlog.INFO ).to.equal(200);
+    expect(errorlog.WARN ).to.equal(300);
+    expect(errorlog.ERROR).to.equal(400);
+    expect(errorlog.OFF  )  .to.equal(Number.MAX_SAFE_INTEGER);
+    expect(errorlog.defaultLog).to.be.a('function');
+    expect(errorlog.defaultLevel).to.equal(errorlog.INFO);
   });
 
   it('should log a simple message', function() {
@@ -80,7 +80,7 @@ describe('Simple log test', function() {
   });
 
   it('should use a category', function() {
-    simplelog('my own category')('A categorized message');
+    errorlog('my own category')('A categorized message');
     expect(stream.check()).to.equal('  LOG - my own category: A categorized message');
   });
 
@@ -88,7 +88,7 @@ describe('Simple log test', function() {
     var logger = new Stream();
 
     log('this should go to the shared logger');
-    simplelog(logger)('different logger');
+    errorlog(logger)('different logger');
 
     expect(stream.check()).to.equal('  LOG - this should go to the shared logger');
     expect(logger.check()).to.equal('  LOG - different logger');
@@ -98,7 +98,7 @@ describe('Simple log test', function() {
     var logger = new Stream();
 
     log('this should go to the shared logger');
-    simplelog({logger: logger, category: 'foobar'})('different logger');
+    errorlog({logger: logger, category: 'foobar'})('different logger');
 
     expect(stream.check()).to.equal('  LOG - this should go to the shared logger');
     expect(logger.check()).to.equal('  LOG - foobar: different logger');
@@ -109,7 +109,7 @@ describe('Simple log test', function() {
     var logger = function(string) { data = string };
 
     log('this should go to the shared logger');
-    simplelog({logger: logger})('different logger');
+    errorlog({logger: logger})('different logger');
 
     expect(stream.check()).to.equal('  LOG - this should go to the shared logger');
     expect(data).to.equal('  LOG - different logger');
@@ -120,7 +120,7 @@ describe('Simple log test', function() {
     var logger = function(string) { data = string };
 
     log('this should go to the shared logger');
-    simplelog({logger: logger, category: 'foobar'})('different logger');
+    errorlog({logger: logger, category: 'foobar'})('different logger');
 
     expect(stream.check()).to.equal('  LOG - this should go to the shared logger');
     expect(data).to.equal('  LOG - foobar: different logger');
@@ -129,7 +129,7 @@ describe('Simple log test', function() {
   it('should honor the ALL level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'all', logger: append});
+    var logger = errorlog({level: 'all', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -141,7 +141,7 @@ describe('Simple log test', function() {
   it('should honor the DEBUG level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'debug', logger: append});
+    var logger = errorlog({level: 'debug', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -153,7 +153,7 @@ describe('Simple log test', function() {
   it('should honor the INFO level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'info', logger: append});
+    var logger = errorlog({level: 'info', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -165,7 +165,7 @@ describe('Simple log test', function() {
   it('should honor the WARN level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'warn', logger: append});
+    var logger = errorlog({level: 'warn', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -177,7 +177,7 @@ describe('Simple log test', function() {
   it('should honor the ERROR level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'error', logger: append});
+    var logger = errorlog({level: 'error', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -189,7 +189,7 @@ describe('Simple log test', function() {
   it('should honor the OFF level', function() {
     var data = [];
     var append = function(more) { data.push(more) };
-    var logger = simplelog({level: 'off', logger: append});
+    var logger = errorlog({level: 'off', logger: append});
     logger.debug('debug');
     logger.info ('info');
     logger.warn ('warn');
@@ -202,21 +202,21 @@ describe('Simple log test', function() {
     var data = [];
     var append = function(more) { data.push(more) };
 
-    var oldLevel = simplelog.defaultLevel;
+    var oldLevel = errorlog.defaultLevel;
     try {
 
-      simplelog.defaultLevel = simplelog.OFF;
-      var logger = simplelog({logger: append});
+      errorlog.defaultLevel = errorlog.OFF;
+      var logger = errorlog({logger: append});
 
       logger('this should not show up');
       expect(data).to.eql([]);
 
-      simplelog.defaultLevel = simplelog.ALL;
+      errorlog.defaultLevel = errorlog.ALL;
       logger('this must show up');
       expect(data).to.eql(['  LOG - this must show up']);
 
     } finally {
-      simplelog.defaultLevel = oldLevel;
+      errorlog.defaultLevel = oldLevel;
     }
   })
 
@@ -226,15 +226,15 @@ describe('Simple log test', function() {
     var append1 = function(more) { data1.push(more) };
     var append2 = function(more) { data2.push(more) };
 
-    var oldLog = simplelog.defaultLog;
+    var oldLog = errorlog.defaultLog;
     try {
-      simplelog.defaultLog = append1;
+      errorlog.defaultLog = append1;
 
-      var logger = simplelog();
+      var logger = errorlog();
 
       logger('logged to data 1');
 
-      simplelog.defaultLog = append2;
+      errorlog.defaultLog = append2;
 
       logger('logged to data 2');
 
@@ -242,9 +242,29 @@ describe('Simple log test', function() {
       expect(data2).to.eql(['  LOG - logged to data 2']);
 
     } finally {
-      simplelog.defaultLog = oldLog;
+      errorlog.defaultLog = oldLog;
     }
   })
+
+  it('should parse every level string', function() {
+    errorlog.defaultLevel = 'ALL'  ;
+    expect(errorlog.defaultLevel).to.equal(-1);
+    errorlog.defaultLevel = 'TRACE';
+    expect(errorlog.defaultLevel).to.equal(0);
+    errorlog.defaultLevel = 'DEBUG';
+    expect(errorlog.defaultLevel).to.equal(100);
+    errorlog.defaultLevel = 'INFO' ;
+    expect(errorlog.defaultLevel).to.equal(200);
+    errorlog.defaultLevel = 'WARN' ;
+    expect(errorlog.defaultLevel).to.equal(300);
+    errorlog.defaultLevel = 'ERROR';
+    expect(errorlog.defaultLevel).to.equal(400);
+    errorlog.defaultLevel = 'FATAL';
+    expect(errorlog.defaultLevel).to.equal(500);
+    errorlog.defaultLevel = 'OFF'  ;
+    expect(errorlog.defaultLevel).to.equal(Number.MAX_SAFE_INTEGER);
+
+  });
 
 });
 
