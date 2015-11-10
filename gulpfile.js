95dbf1330d49948050603471eec55f976813b3ed
Pier Fumagalli <pier@usrz.com>
2015-11-10 16:27:26 +0900

    Switch from Grunt to Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('mocha', function () {
+  return gulp.src('test/**/*.test.js', {read: false})
+    .pipe(mocha({ reporter: 'spec' }));
+});
+
+gulp.task('default', [ 'mocha' ]);
